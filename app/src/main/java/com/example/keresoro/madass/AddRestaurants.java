c223f45f569010f96b2a156b414a0a34bc749afe
Keresoro <38075826+Keresoro@users.noreply.github.com>
2018-04-16 03:35:48 +0100

    Question until 3

---
+++ app/src/main/java/com/example/keresoro/madass/AddRestaurants.java
@@ -0,0 +1,72 @@
+package com.example.keresoro.madass;
+
+/**
+ * Created by Keresoro on 15/04/2018.
+ */
+import android.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.widget.Button;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+
+public class AddRestaurants extends AppCompatActivity implements OnClickListener
+{
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_restaurants);
+
+        TextView restname = (TextView) findViewById(R.id.xrestname);
+        EditText editname = (EditText) findViewById(R.id.xeditname);
+        TextView restad = (TextView) findViewById(R.id.xrestad);
+        EditText editrestad = (EditText) findViewById(R.id.xeditrestad);
+        TextView cusine = (TextView) findViewById(R.id.xcusine);
+        EditText editcusine = (EditText) findViewById(R.id.xeditcusine);
+        TextView rating = (TextView) findViewById(R.id.xrating);
+        EditText editrating = (EditText) findViewById(R.id.xeditrating);
+
+        Button AddRest = (Button)findViewById(R.id.xaddrest);
+        AddRest.setOnClickListener(this);
+    }
+    public void onClick(View v)
+    {
+        //// Edit Texts initialisation
+        TextView restname = (TextView) findViewById(R.id.xrestname);
+        EditText editname = (EditText) findViewById(R.id.xeditname);
+        TextView restad = (TextView) findViewById(R.id.xrestad);
+        EditText editrestad = (EditText) findViewById(R.id.xeditrestad);
+        TextView cusine = (TextView) findViewById(R.id.xcusine);
+        EditText editcusine = (EditText) findViewById(R.id.xeditcusine);
+        TextView rating = (TextView) findViewById(R.id.xrating);
+        EditText editrating = (EditText) findViewById(R.id.xeditrating);
+
+        //Get everything into variables
+        String namerest = editname.getText().toString();
+        String adrest = editrestad.getText().toString();
+        String scusine = editcusine.getText().toString();
+        int srating = Integer.getInteger(editrating.getText().toString());
+
+        if (srating <=10 || srating >0 ) {
+            new AlertDialog.Builder(this).setMessage("Enter Value from 1-10 only!").
+                    setPositiveButton("OK", null).show();
+        } else {
+            Intent intent = new Intent();
+            Bundle bundle = new Bundle();
+
+            // Send variables to main activity via bundle
+            bundle.putString("com.example.restname",namerest);
+            bundle.putString("com.example.raddress", adrest);
+            bundle.putString("com.example.cusine", scusine);
+            bundle.putInt("com.example.rating", srating);
+
+            intent.putExtras(bundle);
+            setResult(RESULT_OK, intent);
+            finish();
+        }
+    }
+}
--- app/src/main/java/com/example/keresoro/madass/MainActivity.java
+++ app/src/main/java/com/example/keresoro/madass/MainActivity.java
@@ -1,13 +1,106 @@
 package com.example.keresoro.madass;
 
 import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.location.LocationManager;
+import android.location.LocationListener;
+import android.location.Location;
+import android.content.Context;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+
+import android.content.Intent;
+import android.preference.PreferenceManager;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+
+public class MainActivity extends Activity implements LocationListener
+{
+    MapView mv;
+
+    Double latitude = 50.9;
+    Double longitude = -1.4;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LocationManager mgr = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        mgr.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+
+
+        mv = (MapView) findViewById(R.id.map1);
+        mv.setBuiltInZoomControls(true);
+        mv.getController().setZoom(16);
+        mv.getController().setCenter(new GeoPoint(latitude, longitude));
+
+        TextView lat = (TextView) findViewById(R.id.lat1);
+        TextView valuelat = (TextView) findViewById(R.id.value1);
+        TextView lon = (TextView) findViewById(R.id.lon1);
+        TextView valuelon = (TextView) findViewById(R.id.value2);
+        //valuelat.setText(lat.toString());
+        //valuelon.setText(lon.toString());
     }
+
+
+    public void onLocationChanged(Location newLoc)
+    {
+        mv.getController().setCenter(new GeoPoint(newLoc.getLatitude(), newLoc.getLongitude()));
+        latitude = Double.parseDouble(String.valueOf(newLoc.getLatitude()));
+        longitude = Double.parseDouble(String.valueOf(newLoc.getLongitude()));
+        Toast.makeText
+                (this, "Location=" +
+                        newLoc.getLatitude()+ " " +
+                        newLoc.getLongitude() , Toast.LENGTH_LONG).show();
+    }
+
+    public void onProviderDisabled(String provider)
+    {
+        Toast.makeText(this, "Provider " + provider +
+                " disabled", Toast.LENGTH_LONG).show();
+    }
+
+    public void onProviderEnabled(String provider)
+    {
+        Toast.makeText(this, "Provider " + provider +
+                " enabled", Toast.LENGTH_LONG).show();
+    }
+
+    public void onStatusChanged(String provider,int status,Bundle extras)
+    {
+
+        Toast.makeText(this, "Status changed: " + status,
+                Toast.LENGTH_LONG).show();
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if(item.getItemId() == R.id.AddRest)
+        {
+            // react to the menu item being selected...
+            return true;
+        }
+        return false;
+    }
+
+
+
 }
